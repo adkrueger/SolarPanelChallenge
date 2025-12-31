356383728b2eab8efd393f10a216dfa85c242f00
Aaron Krueger <akrueger@mathworks.com>
2025-12-31 09:24:03 -0500

    adding M and MLX scaffolding files

---
+++ energyFcn.m
@@ -0,0 +1,19 @@
+function E = energyFcn(theta, r)
+arguments (Input)
+    theta  % first variable is tilt angle; should be a scalar double
+    r  % second variable is aspect ratio; should be a scalar double
+end
+
+arguments (Output)
+    E % output is the energy output of the solar panel; should be a scalar double
+end
+
+% Energy is based on cosine of angle differences and shape efficiency
+% A = ?;  % constant area for a solar panel in m^2
+% nu = ?;  % efficiency function
+% sunIntensity = ?;  % sunlight variation with tilt
+% fr = ?;  % calculate efficiency drop for extreme shapes
+
+% finally, calculate E for output using the variables above
+% E = ?;
+end
